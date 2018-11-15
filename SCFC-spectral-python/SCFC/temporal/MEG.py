ca12f9b3fe9b4abe2db9e4272d1b8cb38ea6a863
Pablo Damasceno <pablodamasceno@gmail.com>
2018-11-15 14:26:44 -0800

    create temporal class

---
+++ SCFC-spectral-python/SCFC/temporal/MEG.py
@@ -0,0 +1,25 @@
+
+
+
+def get_MEG_data(sub_name, ordering, MEGfolder):
+    """Get source localized MEG data and arrange it following ordering method.
+
+    Args:
+        sub_name (str): Name of subject.
+        ordering (arr): Cortical region ordering (e.g. `cortJulia`).
+        MEGfolder (str): Directory for MEG data.
+
+    Returns:
+        MEGdata (arr): MEG data.
+        coords (type):
+
+    """
+    S = loadmat(os.path.join(MEGfolder, sub_name, 'DK_timecourse_20.mat'))
+    MEGdata = S['DK_timecourse']
+    MEGdata = MEGdata[ordering, ]
+    C = loadmat(os.path.join(MEGfolder, sub_name, 'DK_coords_meg.mat'))
+    coords = C['DK_coords_meg']
+    coords = coords[ordering, ]
+    del S, C
+
+    return MEGdata, coords
