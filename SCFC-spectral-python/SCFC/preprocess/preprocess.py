9c8b3e8d4d79e9008c4634427568bbdd2aa6bf16
megstanley <meganstanley4@gmail.com>
2018-11-19 19:36:07 -0800

    Remove ordering to place in new permute module

--- SCFC-spectral-python/SCFC/preprocess/preprocess.py
+++ SCFC-spectral-python/SCFC/preprocess/preprocess.py
@@ -1,6 +1,6 @@
 '''functions to make sure that data is in the right order, or that data is given dictionary
 keys labelling the data according to the brain regions. These are applied prior to any further
-processing.'''
+processing, and as such are not called by other functions directly.'''
 
 import sys, os
 sys.path.append("..")
@@ -83,8 +83,6 @@ def add_key_coords(label_filepath, coordfile):
         coord_dict[keys] = values
     return coord_dict
 
-# def label_connectivity():
-
 def get_desikan(filepath):
     """Parse the desikan atlas ordering for the 68 cortical regions from the csv file,
     or indeed any other csv dictionary.
@@ -114,63 +112,6 @@ def get_desikan(filepath):
                 coords.append([x,y,z])
     return regions, coords
 
-def get_HCP_order(filepath, save=False, fileout = None, cortexstart = 18):
-    """Import the HCP connectome, and create a list with the same order so
-    that input data can be rearranged to compare. The dictionary keys are standardised to single
-    words, lower case only. The HCP is also rearranged so that the cortex comes first,
-    non-cortex parts of the brain are placed after.
-
-    Args:
-        filepath (string): Path to HCP connectome file.
-        save (Boolean): Save output list to file?
-        fileout (string): Location of output list.
-        cortexstart (int): Index of start of cortex in original HCP ordering.
-
-    Returns:
-        List: List of brain regions; HCP rearranged so that the cortex comes first,
-        non-cortex parts of the brain are placed after.
-
-
-    """
-
-    with open(filepath) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        region_order = []
-        for row in csv_reader:
-            if line_count == 0:
-                for cell in row:
-                    index = cell.find('ctx')
-                    if index != -1:
-                        cell = cell[4:6].upper() + cell[index+7:].lower()
-                        region_order.append(cell)
-                    else:
-                        cell = cell
-                        region_order.append(cell)
-                line_count += 1
-            else:
-                break
-
-    #put the non-cortex to the end of the order list
-    order = region_order[cortexstart:]
-    for item in region_order[0:cortexstart]:
-        order.append(item)
-
-    if save:
-        pth.save_hdf5(fileout, order)
-
-    return order
-
-def reorder_connectome(orderfile,connmatfile= None, distmatfile=None,  save = False, cortextstart=18):
-    order = pth.read_hdf5(orderfile)
-    start = order[0]
-    print(order)
-
-    Connectome = np.genfromtxt(conmatfile,delimiter=',', skip_header=1)
-    Distance_matrix = np.genfromtxt(distmatfile,delimiter=',', skip_header=0)
-
-
-
 if __name__ == "__main__":
     #all the testing functions, excuse me for the mess!
 
@@ -189,10 +130,15 @@ if __name__ == "__main__":
     # regions = get_HCP_order(label_filename, save = True, fileout = out_filename)
     # print(regions)
 
-    outlist = 'HCP_list.h5'
-    path = pth.get_sibling_path('dictionaries')
-    filename = os.path.join(path, outlist)
-    reorder_connectome(filename)
+    # outlist = 'HCP_list.h5'
+    # path = pth.get_sibling_path('dictionaries')
+    # datapath = pth.get_sibling_path('data')
+    # confile = os.path.join(datapath, 'mean80_fibercount.csv')
+    # distfile = os.path.join(datapath, 'mean80_fiberlength.csv')
+    # filename = os.path.join(path, outlist)
+    # reorder_connectome(confile, distfile)
+
+
 
     # '''example of the use of this -- preprocessed Chang's data accordingly'''
     # datapath = '/Users/Megan/RajLab/MEG-chang'
