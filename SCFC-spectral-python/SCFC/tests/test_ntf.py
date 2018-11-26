0ecd6799f465ffbc99c9ef7c27248d08910ae9fa
Pablo Damasceno <pablodamasceno@gmail.com>
2018-11-26 09:42:13 -0800

    fix test_distance_matrix test using new Brain functions

--- SCFC-spectral-python/SCFC/tests/test_ntf.py
+++ SCFC-spectral-python/SCFC/tests/test_ntf.py
@@ -1,44 +1,28 @@
 import sys
 sys.path.append("..")
 
-import numpy as np
+#SCFC modules
 from forward import network_transfer as nt
-from utils import path
-from brain import brain
+from utils import functions
+from brain import Brain
+from read import data_reader as dr
+from preprocess import permute as perm
+from utils import path as path
+# from preprocess import filters
 
-new_brain = brain.Brain()
-hcp_dir = path.get_data_path()
-new_brain.set_hcp_connectome(hcp_dir)
-
-# Get structural connectivity matrix + distance between regions
-Cdk_conn = new_brain.Cdk_conn
-Ddk_conn = new_brain.Ddk_conn
-perm_HCP = new_brain.permHCP
-
-# Some other ordering that was in the original code:
-linds = np.concatenate([np.arange(0,34), np.arange(68,77)])
-rinds = np.concatenate([np.arange(34,68), np.arange(77,86)])
-
-q = np.maximum(Cdk_conn[linds,:][:,linds], Cdk_conn[rinds,:][:,rinds])
-q1 = np.maximum(Cdk_conn[linds,:][:,rinds], Cdk_conn[rinds,:][:,linds])
-
-#not sure what this does.
-new_brain.bi_symmetric_c(linds, rinds)
-new_brain.reduce_extreme_dir(Cdk_conn)
-
-C = new_brain.reducedC
-
-default_ntf_params = {'tau_e':0.012, 'tau_i':0.003, 'alpha':1.0,
-                       'speed':5.0, 'gei':4.0, 'gii':1.0, 'tauC':0.006}
+#generic modules
+import numpy as np
+from scipy.signal import lfilter, firls, decimate
+import time
 
 
-fq, ev, Vv, freqresp_out, _ = nt.network_transfer_function(C=C, D=Ddk_conn, parameters = default_ntf_params, w=2*np.pi)
+my_brain = Brain.Brain()
 
+data_dir = path.get_sibling_path('data')
 
-freqresp_out[0]
+my_brain.add_connectome(data_dir)
 
+my_brain.reorder_connectome(my_brain.connectome, my_brain.distance_matrix)
 
-
-
-
-sg.network_transfer_function(C, Ddk_conn, w)
+def test_distance_matrix():
+    assert my_brain.distance_matrix.shape == (86, 86)
