8199832ada5864389cb4fb8a38ff90364b29a9d9
megstanley <meganstanley4@gmail.com>
2018-11-29 12:01:38 -0800

    Add mean_patient function

--- SCFC-spectral-python/SCFC/utils/functions.py
+++ SCFC-spectral-python/SCFC/utils/functions.py
@@ -12,3 +12,24 @@ def mag2db(y):
     """
     dby = 20*np.log10(y)
     return dby
+
+def mean_patient(data, freq_number):
+    """mean patient: for a patient dictionary of brain region MEG data, find the mean over regions.
+
+    Args:
+        data (dict): dictionary keyed by region of data.
+        freq_number (int): Length of the frequency list.
+
+    Returns:
+        array: array of the mean spectrum across all brain regions for the patient.
+
+    """
+    regions = len(list(data.keys()))
+    FMEGmean = np.empty((1, freq_number))
+    dataarray = np.empty((regions, freq_number))
+    i = 0
+    for region in data.keys():
+        dataarray[i,:] = data[region]
+        i += 0
+    FMEGmean = np.mean(dataarray, axis = 0)
+    return FMEGmean
