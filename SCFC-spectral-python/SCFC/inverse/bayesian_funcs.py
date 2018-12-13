71047cde0ba9531ec28afc7ee7b9443fd2f5b098
megstanley <meganstanley4@gmail.com>
2018-12-12 18:38:17 -0800

    Adding likelihood and lnprobs functions (still in development)

--- SCFC-spectral-python/SCFC/inverse/bayesian_funcs.py
+++ SCFC-spectral-python/SCFC/inverse/bayesian_funcs.py
@@ -5,13 +5,13 @@ from inverse import cost
 from forward import runforward as rf
 
 
-def ln_likelihood(theta, brain, fvec, FMEGdata):
+def ln_likelihood_pearson(theta, brain, fvec, FMEGdata):
     """ln_likelihood. The ln likelihood function for MCMC. Takes a brain, parameter dict,
     vector of frequency and data dict (in same order to compare to model), and returns
     negative log likelihood.
 
     Args:
-        theta (dict): Parameters of ntf model as a dictionary (need to change to arrange for MCMC).
+        theta (array): Parameters of ntf model as an array (has to be converted to a dictionary).
         brain (brain class): An instance of the brain class on which to run the ntf model.
         fvec (array): List of frequencies to calculate for.
         FMEGdata (dict): Dictionary of the experimental data.
@@ -21,52 +21,27 @@ def ln_likelihood(theta, brain, fvec, FMEGdata):
 
     """
     #First need to convert parameter array to dictionary required by the run_forward func.
+    if type(theta) != dict:
+        parameters = {'tau_e':theta[0],
+                       'tau_i':theta[1],
+                       'alpha':theta[2],
+                       'speed':theta[3],
+                       'gei':theta[4],
+                       'gii':theta[5],
+                       'tauC':theta[6]
+                       }
+    else:
+        parameters = theta
 
     #first, run the ntf model for this setting of parameters
-    freq_model = rf.run_forward(brain, theta, fvec)
+    freq_model = rf.run_forward(brain, parameters, fvec)
 
     #now calculate the pearson r based error
-    errors, list_errors = cost.pearson_cost(FMEGdata, freq_model)
+    errors, list_errors = cost.pearson_cost_oneminus(FMEGdata, freq_model)
 
     list_errors = np.asarray(list_errors)
 
     #calculate the total negative lnlikelihood based on the errors of the pearson cost function
-    return -0.5*(np.sum(list_errors**2))
+    return -0.5*(np.sum(list_errors))
 
-def ln_uniform_prior(param, range):
-    """ln_uniform_prior: returns log of uniform prior for a parameter,
-    up to a constant -- so returns zero if parameter 'in range' and -inf
-    otherwise.
-
-    Args:
-        param (type): the parameter.
-        range (type): range over which a non-zero prior probability is defined
-        Should be a 2 element list, tuple, or array.
-
-    Returns:
-        type: ln p for the prior probability of the parameter on question.
-
-    """
-    if len(range)%2 != 0:
-        raise TypeError('range given must be of length multiple of two.')
-
-    if range[0]<param<range[1]:
-        return 0.0 #ln prior up to a constant that doesn't matter
-    return -np.inf
-
-def lnprobs(theta, w, y, yerr):
-    """Calculate the log of posterior probability (with no marginalization) for a single
-    parameter set..
-
-    Args:
-        theta (type): the parameters of the model that will be called.
-        w (type): frequency array associated with data points.
-        y (type): real data points.
-        yerr (type): variance of the distribution from which errors are drawn, as array.
-
-    Returns:
-        type: for single theta set, the log(likelihood)xlog(prior).
-
-    """
-
-    return
+#def lnprobs(theta, w, y, yerr):
